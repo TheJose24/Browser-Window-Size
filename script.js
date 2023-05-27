2bc1363994ee0a55a0cc1513bbd1fcf1f9225783
TheJose <joseenrique123438@gmail.com>
2023-05-27 10:33:15 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var ancho = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var alto = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+console.log("Ancho de pantalla: " + ancho + "px");
+console.log("Alto de pantalla: " + alto + "px");
