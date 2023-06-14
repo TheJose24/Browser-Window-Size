7d9e3f1cc9ffd74d9e6b8f9126953186be5f5db6
TheJose <joseenrique123438@gmail.com>
2023-06-13 23:36:45 -0500

    Mejoras en diseño

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-function updateWindowSize() {
+function calcularMedidas() {
+  document.getElementById('cuerpo').style.opacity = '0';
+  document.getElementById('loader').style.display = 'block';
+
+  setTimeout(function() {
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-  
-    document.getElementById('window-width').textContent = windowWidth;
-    document.getElementById('window-height').textContent = windowHeight;
-  }
-  
-  function updateContentSize() {
-    var contentWidth = document.getElementById('content-size').offsetWidth;
-    var contentHeight = document.getElementById('content-size').offsetHeight;
-  
-    document.getElementById('content-width').textContent = contentWidth;
-    document.getElementById('content-height').textContent = contentHeight;
-  }
-  
-  window.addEventListener('resize', function() {
-    updateWindowSize();
-    updateContentSize();
-  });
-  
-  window.addEventListener('DOMContentLoaded', function() {
-    updateWindowSize();
-    updateContentSize();
-  });
-  
+    var contentWidth = document.getElementById('cuerpo').offsetWidth;
+    var contentHeight = document.getElementById('cuerpo').offsetHeight;
+
+    document.getElementById('window-width').textContent = windowWidth + (" px");
+    document.getElementById('window-height').textContent = windowHeight + (" px");
+    document.getElementById('content-width').textContent = contentWidth + (" px");
+    document.getElementById('content-height').textContent = contentHeight + (" px");
+
+    document.getElementById('cuerpo').style.display = 'none';
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('resultados').classList.add('mostrar-resultados');
+    document.getElementById('cuerpo').style.opacity = '1';
+  }, 2000);
+}
